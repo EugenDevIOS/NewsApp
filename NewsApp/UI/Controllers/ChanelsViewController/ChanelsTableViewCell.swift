5ff503f26c2b9af1409eff393176af19f12757ea
EugeneKlyuenkov <dawd>
2021-06-07 15:49:20 +0300

    fix mainTableCell

--- NewsApp/UI/Controllers/ChanelsViewController/ChanelsTableViewCell.swift
+++ NewsApp/UI/Controllers/ChanelsViewController/ChanelsTableViewCell.swift
@@ -4,12 +4,10 @@ class ChanelsTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
 
 }
